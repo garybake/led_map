bc447888e60ca2c902b92a3855f4aa1b06f1d38e
Gary Bake <gary.bake@planixs.com>
2017-03-08 21:24:51 +0000

    Added the files

---
+++ README.md
@@ -0,0 +1,3 @@
+Original from Jeroen Haranging 
+https://www.facebook.com/groups/1499045113679103/permalink/1870320719884872/
+Map with LEDs 1.1
---
+++ map/config.h
@@ -0,0 +1,8 @@
+// Constants.h
+#if !defined(MYLIB_CONSTANTS_H)
+#define MYLIB_CONSTANTS_H 1
+
+static const char ssid[] = "****";
+static const char password[] = "****";
+
+#endif
