2ecaa29212d38516e5de088ea4a400aff1567b33
Gary Bake <gary.bake@planixs.com>
2017-04-21 12:57:32 +0100

    Removed fixed IP and refactored out connect

--- map/map.ino
+++ map/map.ino
@@ -2,25 +2,22 @@
 #include <ESP8266WebServer.h>
 #include "config.h"
 
-ESP8266WebServer server(80);
+//IPAddress ip(192,168,0,30); // fixed IP address
+//IPAddress gateway(192,168,0,255); // network gateway address
+//IPAddress subnet(255,255,255,0); // network subnet mask
 
-IPAddress ip(192,168,0,21); // fixed IP address
-IPAddress gateway(192,168,0,255); // network gateway address
-IPAddress subnet(255,255,255,0); // network subnet mask
+ESP8266WebServer server(8222);
 
-const int LED01 = D1;
-const int LED02 = D2;
-const int LED03 = D3;
-const int LED04 = D4;
+const int PINCOUNT = 9;
+int leds[] = {D0, D1, D2, D3, D4, D5, D6, D7, D8};
 
 void handleRoot() {
-    digitalWrite(LED01, 1);
+//    digitalWrite(LED01, 1);
     server.send(200, "text/plain", "hello from esp8266!");
-    digitalWrite(led, 0);
+//    digitalWrite(led, 0);
 }
 
 void handleNotFound(){
-    digitalWrite(led, 1);
     String message = "File Not Found\n\n";
     message += "URI: ";
     message += server.uri();
@@ -33,14 +30,13 @@ void handleNotFound(){
         message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
     }
     server.send(404, "text/plain", message);
-    digitalWrite(led, 0);
 }
 
 void handleImat(){
-//    digitalWrite(LED01, LOW);
+
     String message = "";
     long locId = -1;
-    int led = LED01;
+    int led;
 
     if (server.arg("location")== ""){     //Parameter not found
     
@@ -48,24 +44,40 @@ void handleImat(){
     
     } else {
         locId = server.arg("location").toInt();
+        led = locId;
         switch (locId) {
+            case 0:
+                message = "At location0\n";
+                break;
             case 1:
-                message = "At location1";
-                led = LED01;
+                message = "At location1\n";
                 break;
             case 2:
-                message = "At location2";
-                led = LED02;
+                message = "At location2\n";
                 break;
             case 3:
-                message = "At location3";
-                led = LED03;
+                message = "At location3\n";
                 break;
             case 4:
-                message = "At location4";
-                led = LED04;
+                message = "At location4\n";
+                break;
+            case 5:
+                message = "At location5\n";
+                break;
+            case 6:
+                message = "At location6\n";
+                break;
+            case 7:
+                message = "At location7\n";
+                break;
+            case 8:
+                message = "At location8\n";
+                break;
+            case 9:
+                message = "At location9\n";
                 break;
             default:
+                led = 0;
                 message = "location Argument = ";
                 message += server.arg("location");
         }
@@ -73,41 +85,45 @@ void handleImat(){
     }
     light_led(led);
     server.send(200, "text/plain", message);     //Returns the HTTP response
-    
-    delay(1000);
-    digitalWrite(LED01, HIGH);
+
 }
 
 void light_led(int led){
-    digitalWrite(LED01, LOW);
-    digitalWrite(LED02, LOW);
-    digitalWrite(LED03, LOW);
-    digitalWrite(LED04, LOW);
 
-    digitalWrite(led, HIGH);
+    for (int i = 0; i < PINCOUNT; i++ ) {
+        digitalWrite(leds[i], LOW);
+    }
+
+    digitalWrite(leds[led], HIGH);
 }
 
 void flashAll(){
-    digitalWrite(LED01, HIGH);
-    digitalWrite(LED02, HIGH);
-    digitalWrite(LED03, HIGH);
-    digitalWrite(LED04, HIGH);
-
-    delay(100);
-    
-    digitalWrite(LED01, LOW);
-    digitalWrite(LED02, LOW);
-    digitalWrite(LED03, LOW);
-    digitalWrite(LED04, LOW);
+    for (int i = 0; i < PINCOUNT; i++ ) {
+        digitalWrite(leds[i], HIGH);
+        delay(200);
+        digitalWrite(leds[i], LOW);
+    }
 }
 
-void setup(void){
-    pinMode(LED01, OUTPUT);
-    pinMode(LED02, OUTPUT);
-    pinMode(LED03, OUTPUT);
-    pinMode(LED04, OUTPUT);
+void printMac(){
+    byte mac[6];
+    WiFi.macAddress(mac);
+    Serial.print("MAC: ");
+    Serial.print(mac[5],HEX);
+    Serial.print(":");
+    Serial.print(mac[4],HEX);
+    Serial.print(":");
+    Serial.print(mac[3],HEX);
+    Serial.print(":");
+    Serial.print(mac[2],HEX);
+    Serial.print(":");
+    Serial.print(mac[1],HEX);
+    Serial.print(":");
+    Serial.println(mac[0],HEX);
+}
 
-    Serial.begin(115200);
+void doConnect(){
+    //WiFi.config(ip, gateway, subnet);
     WiFi.begin(ssid, password);
     Serial.println("");
     
@@ -121,8 +137,20 @@ void setup(void){
     Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+    printMac();
+}
 
-    flashAll();
+void setup(void){
+
+    for (int i = 0; i < PINCOUNT; i++ ) {
+        Serial.println(leds[i]);
+        pinMode(leds[i], OUTPUT);
+    }
+
+    Serial.begin(115200);
+    doConnect();
+
+    flashAll(); flashAll();
 
 //    if (MDNS.begin("esp8266")) {
 //        Serial.println("MDNS responder started");
@@ -132,7 +160,7 @@ void setup(void){
     server.on("/imat", handleImat);
 
     server.on("/inline", [](){
-        server.send(200, "text/plain", "this works as well");
+        server.send(200, "text/plain", "this works as well\n");
     });
 
     server.onNotFound(handleNotFound);
